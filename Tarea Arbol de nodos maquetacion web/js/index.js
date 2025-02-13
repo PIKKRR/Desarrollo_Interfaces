15588a84ab7436a22d4d30c4086615401f305cfc
PIKKKRR <pikkcsgo@gmail.com>
2025-02-13 22:50:38 +0100

    Actualizacion Tarea Arbol de nodos maquetacion web

--- Tarea Arbol de nodos maquetacion web/js/index.js
+++ Tarea Arbol de nodos maquetacion web/js/index.js
@@ -2,7 +2,7 @@ document.getElementById("verMas").addEventListener("click", function() {
     alert("Más información próximamente");
 });
 
-document.querySelectorAll(".buyButton").forEach(button => {
+document.querySelectorAll(".buyButton").forEach((button) => {
     button.addEventListener("click", function() {
         alert("Artículo agregado al carrito");
     });
